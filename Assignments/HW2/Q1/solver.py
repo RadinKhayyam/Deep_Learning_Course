7ea4f3e4814978933133a064f94f9334c87fe0b7
Radin Khayyam <Radin_khayyam@yahoo.com>
2024-12-07 01:50:56 +0330

    Add files via upload

---
+++ Assignments/HW2/Q1/solver.py
@@ -0,0 +1,71 @@
+# You are not allowed to import any other libraries or modules.
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+def train(model, criterion, optimizer, train_dataloader, num_epoch, device):
+    model.to(device)
+    avg_train_loss, avg_train_acc = [], []
+
+    for epoch in range(num_epoch):
+        model.train()
+        batch_train_loss, batch_train_acc = train_one_epoch(model, criterion, optimizer, train_dataloader, device)
+        avg_train_acc.append(np.mean(batch_train_acc))
+        avg_train_loss.append(np.mean(batch_train_loss))
+
+        print(f'\nEpoch [{epoch}] Average training loss: {avg_train_loss[-1]:.4f}, '
+              f'Average training accuracy: {avg_train_acc[-1]:.4f}')
+
+    return model
+
+
+def train_one_epoch(model, criterion, optimizer, train_dataloader, device):
+    batch_train_loss, batch_train_acc = [], []
+
+    #TODO: train the given model for only one batch and store accuracy and loss in batch_train_acc, batch_train_loss repectively.
+    for X_batch, y_batch in train_dataloader:
+        
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+
+        predictions = model(X_batch)
+        loss = criterion(predictions, y_batch)
+
+        _, preds = torch.max(predictions, 1)
+        _, labels = torch.max(y_batch, 1)
+        accuracy = (preds == labels).float().mean().item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        batch_train_loss.append(loss.item())
+        batch_train_acc.append(accuracy)
+
+    return batch_train_loss, batch_train_acc
+
+
+
+def test(model, test_dataloader, device):
+    model.to(device)
+    model.eval()
+    batch_test_acc = []
+
+    #TODO: Test the model on the given test dataset and store accuracy in batch_test_acc. This function return nothing.
+    # Remember you should disable gradient computation during testing.
+    with torch.no_grad():  # Disable gradient computation for testing
+        for X_batch, y_batch in test_dataloader:
+
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+
+            predictions = model(X_batch)
+
+            _, preds = torch.max(predictions, 1)
+            _, labels = torch.max(y_batch, 1)
+            accuracy = (preds == labels).float().mean().item()
+
+            batch_test_acc.append(accuracy)
+
+
+    print(f"The test accuracy is {torch.mean(torch.tensor(batch_test_acc)):.4f}.\n")
